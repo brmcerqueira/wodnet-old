0dc171d5376499c5394e131e76b1844b9aabf7c4
brmcerqueira <brmcerqueira@gmail.com>
2017-02-10 22:51:50 -0300

    ajuste

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 
-var server = require('http').createServer(app);
-
-app.use('/peerjs', ExpressPeerServer(server, { debug: true }));
-
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(8080);
+var server = app.listen(8080);
+
+app.use('/peerjs', ExpressPeerServer(server, { debug: true }));
